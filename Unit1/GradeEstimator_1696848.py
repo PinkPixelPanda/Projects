14045ea05b93b4b58ecf5516d7985d9dc7a4ef10
Britt Perez <1696848@park.edu>
2025-01-26 15:13:05 -0500

    Delete Unit1/GradeEstimator_1696848.py
    Deleting manually uploaded file and will push changes from VS Code moving forward

--- Unit1/GradeEstimator_1696848.py
+++
@@ -1,8 +0,0 @@
-#Ask user for their firt name
-first_name = input("What is your first name? ").title().strip()
-
-#Ask user for their last name
-last_name = input("What is your last name? ").title().strip()
-
-#Print the full name
-print(first_name + " " + last_name)
